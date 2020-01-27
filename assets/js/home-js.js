e5f99fc5de822df966767a0698085a1296b88504
Patricia <roldapatricia@gmail.com>
2020-01-27 23:58:16 +0800

    Final Update

--- assets/js/home-js.js
+++ assets/js/home-js.js
@@ -1,3 +1,50 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })    
+
+// please ignore...
+//   <!-- MOONLIGHT -->
+//                 <div class="col">
+//                     <img src="./assets/images/performances/moonlight/1.jpg"  alt="Buboy Raquitico"   data-toggle="modal" data-target="#moonlight" id="daloy-gallery">
+
+                
+//                     <div class="modal fade" id="moonlight" tabindex="-1" role="dialog" aria-labelledby="artist-title" aria-hidden="true">
+//                         <div class="modal-dialog modal-lg" role="document">
+//                             <div class="modal-content">
+//                                 <div class="modal-header">
+//                                 <h5 class="modal-title" id="artist-title">MOONLIGHT</h5>
+//                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//                                     <span aria-hidden="true">&times;</span>
+//                                 </button>
+//                                 </div>
+    
+//                                     <div class="modal-header">
+//                                     <!-- <h1>MOONLIGHT</h1> -->
+//                                     </div>
+                                    
+//                                     <p class="modal-body">
+//                                         <p class="px-4 text-justify">In Daloy Dance Company upcoming new work, Moonlight, Daloy's dancers are led through a sequence of choreographed investigations that aim to bring them out of the restrictive unity of civility and into a primal state of ecstasy. Structured as an homage to ancient shamanic rites of passage, as well as to the filipino legacy of the queer babaylan, Moonlight is a performed visitation. A wounded healer visits the dancers - hard-worn and wise, beyond species, gender, or time - and endeavours to guide them through a journey toward emancipation. In this season plagued by fear and grief, Moonlight appears and dares us to shake ourselves free from all we know, shed all doubt, and reach for our bliss.</p>
+
+//                                         <p class="mb-0 pb=0 px-4"><span class="font-weight-bold">Direction and choreography:</span> Ea Torrado</p>
+//                                         <p class="mb-0 px-4 pb-2"><span class="font-weight-bold">Performance:</span> Daloy Dance Company and Ea Torrado</p>
+
+//                                         <img src="./assets/images/performances/moonlight/1.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/2.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/3.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/4.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/5.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/6.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/7.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/8.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/9.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/10.jpg" class="img-fluid" alt="Moonlight">
+//                                         <img src="./assets/images/performances/moonlight/11.jpg" class="img-fluid" alt="Moonlight">
+
+    
+//                                     <div class="modal-footer">
+//                                         <img src="./assets/images/daloy-logo/DDC.jpg" class="img-fluid" alt="Daloy Dance Company">
+//                                     </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
